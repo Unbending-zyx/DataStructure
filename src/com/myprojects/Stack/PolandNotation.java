2dda085466756e3f51c16e8b1b5a3977def69cdb
zhaoyuxiao <zhaoyuxiao@koodle.cn>
2019-12-11 14:44:50 +0800

    feat:完善逆波兰表达式计算机，添加中缀表达式转换成后缀表达式（逆波兰表达式）的方法

--- src/com/myprojects/Stack/PolandNotation.java
+++ src/com/myprojects/Stack/PolandNotation.java
@@ -8,10 +8,67 @@ public class PolandNotation {
     public static void main(String[] args) {
         //先定义一个逆波兰表达式
         //  (3+4)*5-6   ==>  3 4 + 5 * 6 -   对应的逆波兰表达式
-        String suffixExpression="34+5*6-";
-        char[] sufCharArray=suffixExpression.toCharArray();
+        String suffixExpression="(3+4)*5-6";
+        char[] sufCharArray=change(suffixExpression).toCharArray();
         int a=calculation(sufCharArray);
         System.out.println(a);
+        System.out.println(change("1+((2+3)*4)-5"));
+    }
+
+    //中缀表达式转后缀表达式
+    //  例:  由  1+((2+3)*4)-5   ==> 123+4*+5-
+    public static String change(String str){
+        Stack s1=new Stack();
+        Stack s2=new Stack();
+        StringBuilder result=new StringBuilder();
+        char[] charArray=str.toCharArray();
+        for (char c:charArray){
+            if (Character.isDigit(c)){
+                s2.push(c);
+                continue;
+            }
+            if (c=='('){
+                s1.push(c);
+                continue;
+            }else if(c==')'){
+                while((char)s1.peek()!='('){
+                    s2.push(s1.pop());
+                }
+                s1.pop();
+                continue;
+            }else{
+                isOper(c,s1,s2);
+            }
+        }
+        while(!s1.empty()){
+            s2.push(s1.pop());
+        }
+        while(!s2.empty()){
+            result.append(s2.pop());
+        }
+        return result.reverse().toString();
+    }
+
+    public static void isOper(char c,Stack s1,Stack s2){
+        if (s1.empty() || (char)s1.peek()=='('){
+            s1.push(c);
+        }else if (priority(c)>priority((char)s1.peek())){
+            s1.push(c);
+        }else{
+            s2.push(s1.pop());
+            isOper(c, s1, s2);
+        }
+    }
+
+    //判断传入符号的优先级
+    public static int priority(char oper){
+        if (oper=='*' || oper=='/'){
+            return 1;
+        }else if (oper=='+' || oper=='-'){
+            return 0;
+        }else{
+            return -1;
+        }
     }
 
 
